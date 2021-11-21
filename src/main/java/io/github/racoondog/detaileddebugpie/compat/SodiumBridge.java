c7f870679e4a839f264d2d814a7f90fb3133412a
RacoonDog <32882447+RacoonDog@users.noreply.github.com>
2021-11-21 02:08:50 -0500

    start sodium compat

---
+++ src/main/java/io/github/racoondog/detaileddebugpie/compat/SodiumBridge.java
@@ -0,0 +1,54 @@
+package io.github.racoondog.detaileddebugpie.compat;
+
+import me.jellysquid.mods.sodium.client.SodiumClientMod;
+import me.jellysquid.mods.sodium.client.render.SodiumWorldRenderer;
+import me.jellysquid.mods.sodium.client.util.NativeBuffer;
+import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.util.Formatting;
+
+import java.lang.management.ManagementFactory;
+import java.util.Collection;
+import java.util.Collections;
+
+@SuppressWarnings("StringBufferReplaceableByString")
+public class SodiumBridge {
+    public static String getFormattedVersionText() {
+        if (FabricLoader.getInstance().isModLoaded("sodium")) {
+            String version = SodiumClientMod.getVersion();
+            Formatting color;
+            if (version.endsWith("-dirty")) {
+                color = Formatting.RED;
+            } else if (version.contains("+rev.")) {
+                color = Formatting.LIGHT_PURPLE;
+            } else {
+                color = Formatting.GREEN;
+            }
+            return new StringBuilder(String.valueOf(color)).append(version).toString();
+        } else {
+            return "";
+        }
+    }
+    public static Collection<String> getMemoryDebugStrings() {
+        if (FabricLoader.getInstance().isModLoaded("sodium")) {
+            SodiumWorldRenderer renderer = SodiumWorldRenderer.instanceNullable();
+            if (renderer != null) {
+                return renderer.getMemoryDebugStrings();
+            } else {
+                return Collections.singleton("");
+            }
+        } else {
+            return Collections.singleton("");
+        }
+    }
+    public static long getNativeMemoryString() {
+        if (FabricLoader.getInstance().isModLoaded("sodium")) {
+            return toMiB(ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage().getUsed() + NativeBuffer.getTotalAllocated());
+        } else {
+            return 0;
+        }
+    }
+
+    private static long toMiB(long bytes) {
+        return bytes / 1024L / 1024L;
+    }
+}
--- src/main/java/io/github/racoondog/detaileddebugpie/impl/OptimizedRightText.java
+++ src/main/java/io/github/racoondog/detaileddebugpie/impl/OptimizedRightText.java
@@ -9,12 +9,12 @@ import net.minecraft.client.MinecraftClient;
 import net.minecraft.util.Formatting;
 
 import java.util.ArrayList;
-import java.util.Collection;
 
 @SuppressWarnings("StringBufferReplaceableByString")
 @Environment(EnvType.CLIENT)
 public class OptimizedRightText {
     private static final MinecraftClient CLIENT = MinecraftClient.getInstance();
+    private static final String UNDERLINE = String.valueOf(Formatting.UNDERLINE.getCode());
     private static final ArrayList<String> LIST = Lists.newArrayList(
             new StringBuilder("Java: ").append(System.getProperty("java.version")).append(' ').append(CLIENT.is64Bit() ? "64" : "32").append("bit").toString(), //JAVA
             "", //MEMORY
@@ -27,6 +27,22 @@ public class OptimizedRightText {
             GlDebugInfo.getVersion() //VERSION
     );
 
+    private static final ArrayList<String> SODIUM_LIST = Lists.newArrayList(
+            new StringBuilder("Java: ").append(System.getProperty("java.version")).append(' ').append(CLIENT.is64Bit() ? "64" : "32").append("bit").toString(),
+            "", //MEMORY
+            new StringBuilder("Allocated: ").append(Runtime.getRuntime().totalMemory() * 100L / Runtime.getRuntime().maxMemory()).append("% ").append(toMiB(Runtime.getRuntime().totalMemory())).append("MB").toString(),
+            "", //OFF-HEAP
+            "", //VOID
+            new StringBuilder("CPU: ").append(GlDebugInfo.getCpuInfo()).toString(), //CPU
+            "", //VOID
+            "", //DISPLAY
+            GlDebugInfo.getRenderer(), //RENDERER
+            GlDebugInfo.getVersion(), //VERSION
+            "", //VOID
+            "Sodium Renderer",
+            new StringBuilder(UNDERLINE).append(SodiumBridge.getFormattedVersionText()).toString()
+    );
+
     private static final String MEMORY = new StringBuilder("Mem: % 2d%% %03d/").append(toMiB(Runtime.getRuntime().maxMemory())).append("MB").toString();
     private static final String DISPLAY = new StringBuilder("Display: %dx%d (").append(GlDebugInfo.getVendor()).append(')').toString();
 
@@ -37,12 +53,25 @@ public class OptimizedRightText {
     private static String parseDisplay() {
         return String.format(DISPLAY, CLIENT.getWindow().getFramebufferWidth(), CLIENT.getWindow().getFramebufferHeight());
     }
+    private static String parseOffheap() {
+        return String.format("Off-Heap: +%dMB", SodiumBridge.getNativeMemoryString());
+    }
 
     @SuppressWarnings("unchecked")
     public static ArrayList<String> generate() {
         final ArrayList<String> out = (ArrayList<String>) LIST.clone(); //Clone instead of overwrite to prevent memory leak
         out.set(1, parseMemory());
         out.set(6, parseDisplay());
+        return out;
+    }
+
+    @SuppressWarnings("unchecked")
+    public static ArrayList<String> generateSodium() {
+        final ArrayList<String> out = (ArrayList<String>) SODIUM_LIST.clone();
+        out.set(1, parseMemory());
+        out.set(3, parseOffheap());
+        out.set(7, parseDisplay());
+        out.addAll(SodiumBridge.getMemoryDebugStrings());
         return out;
     }
 
